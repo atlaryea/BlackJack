92668fbb950c1849d4e3333cf3b20aa2ed0ff727
Laryea Alfred Tetteh <123911542+atlaryea@users.noreply.github.com>
2023-10-03 11:51:51 +0000

    Add files via upload
    Javasript code for the blackjack

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+let firstCard = 10
+let secondCard = 10
+let sum = firstCard + secondCard
+let hasBlackjack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementsByClassName("message-el")
+console.log(messageEl)
 
+//store the message-el paragraph in a variable 
+function startGame(){
+    if (sum < 21){
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21){
+        message = "You've got Blackjack!"
+        hasBlackjack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+}
+console.log(message)
+
+//Display the message
+messageEl.textContent = message
+
+//cash out
