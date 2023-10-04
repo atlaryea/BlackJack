0fc3f8583ec322052e3f780e47a7b2f8f316152b
Laryea Alfred Tetteh <123911542+atlaryea@users.noreply.github.com>
2023-10-04 14:24:42 +0000

    Update index.js
    Added new functions for new card and textcontent for Cards and sum

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 let firstCard = 10
-let secondCard = 12
+let secondCard = 4
+let cards = [firstCard, secondCard] //array
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-
+function startGame() {
+    runGame()
+}
 //store the message-el paragraph in a variable 
-function startGame(){
+function runGame() {
+
+    cardsEl.textContent = "Cards: "
+    for (let i=0; i<cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
     if (sum < 21){
         message = "Do you want to draw a new card?"
     } else if (sum === 21){
@@ -19,4 +31,15 @@ function startGame(){
         isAlive = false
     }
     messageEl.textContent = message
+}
+
+function newCard() {
+
+    let card = 7
+
+    sum += card
+
+    cards.push(card)
+
+    runGame()
 }
